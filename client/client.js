01a696b72daa97ab05517e036bf465e4950a34a2
techtechnikov <techtechnikov@ya.ru>
2021-03-13 18:12:49 +0300

    update client file

--- client/client.js
+++ client/client.js
@@ -1,1 +1,12 @@
-alert("test");
+function showFile(input) {
+  let reader = new FileReader();
+  let file = input.files[0];
+  reader.readAsText(file);
+  reader.onload = function() {
+    console.log(reader.result);
+  };
+  reader.onerror = function() {
+    console.log(reader.error);
+  };
+  alert(`File name: ${file.name}`); // например, my.png
+}
