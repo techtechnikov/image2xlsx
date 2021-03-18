001230f7cade7c4e29d16101cb5ffc0efef3d19d
techtechnikov <techtechnikov@ya.ru>
2021-03-18 19:15:05 +0300

    update

--- src/handling.py
+++ src/handling.py
@@ -1,5 +1,14 @@
+import os
+from neuralnetwork import NeuralNetwork
 
 def handle(path):
     print(path)
     for i in range(1000000):pass
     return ('Иван Иванов', {('a', '1'), ('b', '2'), ('c', '3')})
+
+if os.path.exists('usermode.lock'):    #checking mode for 'user' or 'develop'
+    mode = 'user'
+else:
+    mode = 'develop'
+
+n = NeuralNetwork.import_from_file('neuralnetwork.json')
