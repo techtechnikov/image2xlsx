76c09a12bf2809e3971c2daa4d5b848859dd5ec9
techtechnikov <techtechnikov@ya.ru>
2021-03-19 15:54:53 +0300

    update main program

--- src/handling.py
+++ src/handling.py
@@ -4,11 +4,14 @@ from neuralnetwork import NeuralNetwork
 def handle(path):
     print(path)
     for i in range(1000000):pass
-    return ('Иван Иванов', {('a', '1'), ('b', '2'), ('c', '3')})
+    return ('Иван Иванов', 7, {('1', 'a'), ('2', 'b'), ('3', 'c')})
 
 if os.path.exists('usermode.lock'):    #checking mode for 'user' or 'develop'
     mode = 'user'
 else:
     mode = 'develop'
 
-n = NeuralNetwork.import_from_file('neuralnetwork.json')
+if mode == 'user1':
+    n = NeuralNetwork.import_from_file('neuralnetwork.json') # loading learned nueral network
+else:
+    n = NeuralNetwork(43)
